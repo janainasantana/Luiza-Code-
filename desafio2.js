2a699f6c92e0a13f80c0f4f874b3fde7a9fe780a
Janaina do Carmo Santana <janacsantana@gmail.com>
2021-08-19 16:16:44 -0300

    envio de desafio luisacode

---
+++ desafio2.js
@@ -0,0 +1,22 @@
+//fazer a mesma troca usando x e y sem que z exista e sem criar outra variável
+
+let x = 5;
+let y = 8;
+
+[x, y] = [y, x]; 
+
+console.log(`x = ${x}`);
+console.log(`y = ${y}`);
+
+// Usando Auxiliar
+
+//let x = 5;
+//let y = 8;
+
+//let aux = x;
+
+//x = y;
+//y = aux;
+
+//console.log("x = " + x);
+//console.log("y = " + y);
